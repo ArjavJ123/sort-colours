104181d24d52c3ae2b383e9890dd38cd1bcc35ec
ArjavJ123 <75328060+ArjavJ123@users.noreply.github.com>
2022-08-31 13:43:06 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int c0=0, c1 =0, c2 =0;
+        for(int i=0 ;i <nums.size(); i++) {
+            if(nums[i] == 0) {
+                c0++;
+            }
+            else if(nums[i] == 1) {
+                c1++;
+            }
+            else {
+                c2++;
+            }
+        }
+        int i=0;
+        for(; i<c0; i++) {
+            nums[i] = 0;
+        }
+        for(; i<c0+c1; i++) {
+            nums[i] = 1;
+        }
+        for(; i<c0+c1+c2; i++) {
+            nums[i] = 2;
+        }
+    }
+};
